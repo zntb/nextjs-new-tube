a7600d683b60108fae85ba4e67c07eefcd3f13ca
zntb <zenetibi@yahoo.com>
2025-03-13 13:49:44 +0200

    Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <p className='font-bold text-rose-500'>Hello World</p>
+      <Button variant='destructive'>Button</Button>
+    </div>
+  );
+}
