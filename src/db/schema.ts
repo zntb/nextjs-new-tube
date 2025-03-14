cd972b0c5b34abc3fa8dba452696d71de9d6530c
zntb <zntbdev@proton.me>
2025-03-14 12:33:21 +0200

    Database setup

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    clerkId: text('clerk_id').unique().notNull(),
+    name: text('name').notNull(),
+    // TODO: Add banner fields
+    imageUrl: text('image_url').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  t => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+);
