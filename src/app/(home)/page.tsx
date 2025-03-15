5653ddca5bca6aae09808f94fddc84a941996833
zntb <zenetibi@yahoo.com>
2025-03-15 17:32:25 +0200

    Fetch categories with trpc

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,24 @@
+import { HomeView } from '@/modules/home/ui/views/home-view';
 import { HydrateClient, trpc } from '@/trpc/server';
-import { PageClient } from './client';
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 
-export default async function Home() {
-  void trpc.hello({ text: 'world' });
+export const dynamic = 'force-dynamic';
+
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ErrorBoundary fallback={<div>Something went wrong</div>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
-}
+};
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,20 +1,8 @@
-import { z } from 'zod';
-import { createTRPCRouter, protectedProcedure } from '../init';
+import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
-  hello: protectedProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query(opts => {
-      console.log({ dbUser: opts.ctx.user });
-
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  categories: categoriesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
