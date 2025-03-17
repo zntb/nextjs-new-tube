e13ded12741857e72478ea3edc6e2bff2023aaf3
zntb <zenetibi@yahoo.com>
2025-03-17 14:45:00 +0200

    Fetch Video categories

--- src/modules/categories/server/procedures.ts
+++ src/modules/categories/server/procedures.ts
@@ -1,9 +1,11 @@
 import { db } from '@/db';
 import { categories } from '@/db/schema';
 import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+// import { TRPCError } from '@trpc/server';
 
 export const categoriesRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
+    // throw new TRPCError({ code: 'BAD_REQUEST' });
     const data = await db.select().from(categories);
 
     return data;
