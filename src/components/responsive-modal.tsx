7c80f350083bcf1eb141611e4c24aa565184bc93
zntb <zntbdev@proton.me>
2025-03-18 16:02:11 +0200

    Mux integration

---
+++ src/components/responsive-modal.tsx
@@ -0,0 +1,53 @@
+import { useIsMobile } from '@/hooks/use-mobile';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer';
+
+interface ResponsiveModalProps {
+  children: React.ReactNode;
+  open: boolean;
+  title: string;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveModal = ({
+  open,
+  title,
+  onOpenChange,
+  children,
+}: ResponsiveModalProps) => {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+          </DrawerHeader>
+          {children}
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,24 +1,33 @@
 import { db } from '@/db';
 import { videos } from '@/db/schema';
+import { mux } from '@/lib/mux';
 import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
-// import { TRPCError } from '@trpc/server';
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
 
-    // throw new TRPCError({ code: 'BAD_REQUEST' });
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ['public'],
+      },
+      cors_origin: '*', // TODO: In production set to your url
+    });
 
     const [video] = await db
       .insert(videos)
       .values({
         userId,
         title: 'Untitled',
+        muxStatus: 'waiting',
+        muxUploadId: upload.id,
       })
       .returning();
 
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
