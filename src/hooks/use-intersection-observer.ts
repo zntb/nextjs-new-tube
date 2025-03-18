28b77f0df4dde2562fd947597cded5a701294b6e
zntb <zenetibi@yahoo.com>
2025-03-18 11:21:22 +0200

    Add Infinite loading

---
+++ src/hooks/use-intersection-observer.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useIntersectionObserver = (options?: IntersectionObserverInit) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    }, options);
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [options]);
+
+  return { targetRef, isIntersecting };
+};
