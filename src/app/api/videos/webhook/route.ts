b3392158d6a933a91e9934930deae6696d5f724b
zntb <zenetibi@yahoo.com>
2025-03-20 15:02:50 +0200

    Finish Video thumbnails

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -1,5 +1,6 @@
 import { eq } from 'drizzle-orm';
 import { headers } from 'next/headers';
+import { UTApi } from 'uploadthing/server';
 import {
   VideoAssetCreatedWebhookEvent,
   VideoAssetErroredWebhookEvent,
@@ -75,10 +76,22 @@ export const POST = async (request: Request) => {
         return new Response('Missing playback ID', { status: 400 });
       }
 
-      const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
-      const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
-
+      const tempThumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+      const tempPreviewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
       const duration = data.duration ? Math.round(data.duration * 1000) : 0;
+
+      const utapi = new UTApi();
+      const [uploadedThumbnail, uploadedPreview] =
+        await utapi.uploadFilesFromUrl([tempThumbnailUrl, tempPreviewUrl]);
+
+      if (!uploadedThumbnail.data || !uploadedPreview.data) {
+        return new Response('Failed to upload thumbnail or preview', {
+          status: 500,
+        });
+      }
+
+      const { key: thumbnailKey, url: thumbnailUrl } = uploadedThumbnail.data;
+      const { key: previewKey, url: previewUrl } = uploadedPreview.data;
 
       await db
         .update(videos)
@@ -87,7 +100,9 @@ export const POST = async (request: Request) => {
           muxPlaybackId: playbackId,
           muxAssetId: data.id,
           thumbnailUrl,
+          thumbnailKey,
           previewUrl,
+          previewKey,
           duration,
         })
         .where(eq(videos.muxUploadId, data.upload_id));
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -64,7 +64,9 @@ export const videos = pgTable('videos', {
   muxTrackId: text('mux_track_id').unique(),
   muxTrackStatus: text('mux_track_status'),
   thumbnailUrl: text('thumbnail_url'),
+  thumbnailKey: text('thumbnail_key'),
   previewUrl: text('preview_url'),
+  previewKey: text('preview_key'),
   duration: integer('duration').default(0).notNull(),
   visibility: videoVisibility('visibility').default('private').notNull(),
   userId: uuid('user_id')
