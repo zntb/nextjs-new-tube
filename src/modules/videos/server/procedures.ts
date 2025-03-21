a945d61d868db317eadd25fc6eb8113ed3fd05ec
zntb <zntbdev@proton.me>
2025-03-21 11:05:00 +0200

    Add upstash workflows to update video title

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,6 +1,7 @@
 import { db } from '@/db';
 import { videos, videoUpdateSchema } from '@/db/schema';
 import { mux } from '@/lib/mux';
+import { workflow } from '@/lib/workflow';
 import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
 import { TRPCError } from '@trpc/server';
 import { and, eq } from 'drizzle-orm';
@@ -8,6 +9,19 @@ import { UTApi } from 'uploadthing/server';
 import { z } from 'zod';
 
 export const videosRouter = createTRPCRouter({
+  generateThumbnail: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+        body: { userId, videoId: input.id },
+        // retries: 3,
+      });
+
+      return workflowRunId;
+    }),
   restoreThumbnail: protectedProcedure
     .input(z.object({ id: z.string().uuid() }))
     .mutation(async ({ ctx, input }) => {
