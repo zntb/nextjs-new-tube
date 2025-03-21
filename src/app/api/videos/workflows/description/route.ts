4521d7fc2ae28238cc28138f722e4cba92fdbc0b
zntb <zenetibi@yahoo.com>
2025-03-21 12:15:39 +0200

    Add AI background jobs

--- src/app/api/videos/workflows/description/route.ts
+++ src/app/api/videos/workflows/description/route.ts
@@ -8,14 +8,13 @@ interface InputType {
   videoId: string;
 }
 
-const TITLE_SYSTEM_PROMPT = `Your task is to generate an SEO-focused title for a YouTube video based on its transcript. Please follow these guidelines:
+const DESCRIPTION_SYSTEM_PROMPT = `Your task is to summarize the transcript of a video. Please follow these guidelines:
 
-- Be concise but descriptive, using relevant keywords to improve discoverability.
-- Highlight the most compelling or unique aspect of the video content.
-- Avoid jargon or overly complex language unless it directly supports searchability.
-- Use action-oriented phrasing or clear value propositions where applicable.
-- Ensure the title is 3-8 words long and no more than 100 characters.
-- ONLY return the title as plain text. Do not add quotes or any additional formatting.`;
+- Be brief. Condense the content into a summary that captures the key points and main ideas without losing important details.
+- Avoid jargon or overly complex language unless necessary for the context.
+- Focus on the most critical information, ignoring filler, repetitive statements, or irrelevant tangents.
+- ONLY return the summary, no other text, annotations, or comments.
+- Aim for a summary that is 3-5 sentences long and no more than 200 characters.`;
 
 export const { POST } = serve(async context => {
   const input = context.requestPayload as InputType;
@@ -47,7 +46,7 @@ export const { POST } = serve(async context => {
     return text;
   });
 
-  const { body } = await context.api.openai.call('generate-title', {
+  const { body } = await context.api.openai.call('generate-description', {
     token: process.env.OPENAI_API_KEY!,
     operation: 'chat.completions.create',
     body: {
@@ -55,7 +54,7 @@ export const { POST } = serve(async context => {
       messages: [
         {
           role: 'system',
-          content: TITLE_SYSTEM_PROMPT,
+          content: DESCRIPTION_SYSTEM_PROMPT,
         },
         {
           role: 'user',
@@ -65,16 +64,16 @@ export const { POST } = serve(async context => {
     },
   });
 
-  const title = body.choices[0]?.message.content;
+  const description = body.choices[0]?.message.content;
 
-  if (!title) {
+  if (!description) {
     throw new Error('Bad request');
   }
 
   await context.run('update-video', async () => {
     await db
       .update(videos)
-      .set({ title: title || video.title })
+      .set({ description: description || video.description })
       .where(and(eq(videos.id, video.id), eq(videos.userId, video.userId)));
   });
 });
