173f69c05dc8b2570db3e40c2b5101734bf70073
zntb <zntbdev@proton.me>
2025-03-24 12:49:08 +0200

    Implement Subscriptions

---
+++ src/modules/subscriptions/hooks/use-subscription.ts
@@ -0,0 +1,69 @@
+import { toast } from 'sonner';
+import { useClerk } from '@clerk/nextjs';
+import { trpc } from '@/trpc/client';
+
+interface UseSubscriptionProps {
+  userId: string;
+  isSubscribed: boolean;
+  fromVideoId?: string;
+}
+
+export const useSubscription = ({
+  userId,
+  isSubscribed,
+  fromVideoId,
+}: UseSubscriptionProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const subscribe = trpc.subscriptions.create.useMutation({
+    onSuccess: () => {
+      toast.success('Subscribed');
+      // TODO: re-invalidate subscriptions.getMany, users.getOne
+
+      if (fromVideoId) {
+        utils.videos.getOne.invalidate({ id: fromVideoId });
+      }
+    },
+    onError: error => {
+      toast.error('Something went wrong');
+
+      if (error.data?.code === 'UNAUTHORIZED') {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const unsubscribe = trpc.subscriptions.remove.useMutation({
+    onSuccess: () => {
+      toast.success('Unsubscribed');
+      // TODO: re-invalidate subscriptions.getMany, users.getOne
+
+      if (fromVideoId) {
+        utils.videos.getOne.invalidate({ id: fromVideoId });
+      }
+    },
+    onError: error => {
+      toast.error('Something went wrong');
+
+      if (error.data?.code === 'UNAUTHORIZED') {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const isPending = subscribe.isPending || unsubscribe.isPending;
+
+  const onClick = () => {
+    if (isSubscribed) {
+      unsubscribe.mutate({ userId });
+    } else {
+      subscribe.mutate({ userId });
+    }
+  };
+
+  return {
+    isPending,
+    onClick,
+  };
+};
