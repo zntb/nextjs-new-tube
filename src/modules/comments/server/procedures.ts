c977a2e9cc51c92f0d625057ca6bc755a66ddb13
zntb <zntbdev@proton.me>
2025-03-25 10:00:00 +0200

    Add Comments infinite loading

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { eq, getTableColumns } from 'drizzle-orm';
+import { eq, getTableColumns, desc, and, or, lt, count } from 'drizzle-orm';
 import {
   baseProcedure,
   createTRPCRouter,
@@ -7,8 +7,28 @@ import {
 } from '@/trpc/init';
 import { db } from '@/db';
 import { comments, users } from '@/db/schema';
+import { TRPCError } from '@trpc/server';
 
 export const commentsRouter = createTRPCRouter({
+  remove: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      const { id: userId } = ctx.user;
+
+      const [deletedComment] = await db
+        .delete(comments)
+        .where(and(eq(comments.id, id), eq(comments.userId, userId)))
+        .returning();
+
+      if (!deletedComment) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+        });
+      }
+
+      return deletedComment;
+    }),
   create: protectedProcedure
     .input(z.object({ videoId: z.string().uuid(), value: z.string() }))
     .mutation(async ({ ctx, input }) => {
@@ -23,19 +43,67 @@ export const commentsRouter = createTRPCRouter({
       return createdComment;
     }),
   getMany: baseProcedure
-    .input(z.object({ videoId: z.string().uuid() }))
+    .input(
+      z.object({
+        videoId: z.string().uuid(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      }),
+    )
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, cursor, limit } = input;
 
-      const data = await db
-        .select({
-          ...getTableColumns(comments),
-          user: users,
-        })
-        .from(comments)
-        .where(eq(comments.videoId, videoId))
-        .innerJoin(users, eq(comments.userId, users.id));
+      const [totalData, data] = await Promise.all([
+        db
+          .select({
+            count: count(),
+          })
+          .from(comments)
+          .where(eq(comments.videoId, videoId)),
 
-      return data;
+        db
+          .select({
+            ...getTableColumns(comments),
+            user: users,
+          })
+          .from(comments)
+          .where(
+            and(
+              eq(comments.videoId, videoId),
+              cursor
+                ? or(
+                    lt(comments.updatedAt, cursor.updatedAt),
+                    and(
+                      eq(comments.updatedAt, cursor.updatedAt),
+                      lt(comments.id, cursor.id),
+                    ),
+                  )
+                : undefined,
+            ),
+          )
+          .innerJoin(users, eq(comments.userId, users.id))
+          .orderBy(desc(comments.updatedAt), desc(comments.id))
+          .limit(limit + 1),
+      ]);
+
+      const hasMore = data.length > limit;
+      // Remove the last item if there is more data
+      const items = hasMore ? data.slice(0, -1) : data;
+      // Set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? { id: lastItem.id, updatedAt: lastItem.updatedAt }
+        : null;
+
+      return {
+        totalCount: totalData[0].count,
+        items,
+        nextCursor,
+      };
     }),
 });
--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,13 +1,47 @@
 import Link from 'next/link';
+import { trpc } from '@/trpc/client';
 import { CommentsGetManyOutput } from '../../types';
 import { UserAvatar } from '@/components/user-avatar';
 import { formatDistanceToNow } from 'date-fns';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from 'lucide-react';
+import { useAuth, useClerk } from '@clerk/nextjs';
+import { toast } from 'sonner';
 
 interface CommentItemProps {
-  comment: CommentsGetManyOutput[number];
+  comment: CommentsGetManyOutput['items'][number];
 }
 
 export const CommentItem = ({ comment }: CommentItemProps) => {
+  const clerk = useClerk();
+  const { userId } = useAuth();
+  const utils = trpc.useUtils();
+
+  const remove = trpc.comments.remove.useMutation({
+    onSuccess: () => {
+      toast.success('Comment deleted');
+      utils.comments.getMany.invalidate({ videoId: comment.videoId });
+    },
+
+    onError: error => {
+      toast.error('Something went wrong');
+
+      if (error.data?.code === 'UNAUTHORIZED') {
+        clerk.openSignIn();
+      }
+    },
+  });
+
+  const handleRemove = () => {
+    remove.mutate({ id: comment.id });
+  };
+
   return (
     <div>
       <div className='flex gap-4'>
@@ -32,7 +66,27 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
             </div>
           </Link>
           <p className='text-sm'>{comment.value}</p>
+          {/* TODO: Reactions */}
         </div>
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' size='icon' className='size-8'>
+              <MoreVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuItem onClick={() => {}}>
+              <MessageSquareIcon className='size-4 mt-1' />
+              Reply
+            </DropdownMenuItem>
+            {comment.user.clerkId === userId && (
+              <DropdownMenuItem onClick={handleRemove}>
+                <Trash2Icon className='size-4' />
+                Delete
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
