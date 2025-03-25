a1875e99b1f3a05372c4b070d22fafdba60d3f8d
zntb <zenetibi@yahoo.com>
2025-03-25 10:00:00 +0200

    Add Comments infinite loading

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LIMIT } from '@/constants';
 import { VideoView } from '@/modules/videos/ui/views/video-view';
 import { HydrateClient, trpc } from '@/trpc/server';
 
@@ -9,8 +10,11 @@ const SingleVideoPage = async ({ params }: PageProps) => {
   const { videoId } = await params;
 
   void trpc.videos.getOne.prefetch({ id: videoId });
-  // TODO: don't forget to change to 'prefetchInfinite'
-  void trpc.comments.getMany.prefetch({ videoId: videoId });
+
+  void trpc.comments.getMany.prefetchInfinite({
+    videoId,
+    limit: DEFAULT_LIMIT,
+  });
 
   return (
     <HydrateClient>
--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { InfiniteScroll } from '@/components/infinite-scroll';
+import { DEFAULT_LIMIT } from '@/constants';
 import { CommentForm } from '@/modules/comments/ui/components/comment-form';
 import { CommentItem } from '@/modules/comments/ui/components/comment-item';
 import { trpc } from '@/trpc/client';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Loader2Icon } from 'lucide-react';
 
 interface CommentsSectionProps {
   videoId: string;
@@ -12,7 +15,7 @@ interface CommentsSectionProps {
 
 export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<CommentsSectionSkeleton />}>
       <ErrorBoundary fallback={<p>Error...</p>}>
         <CommentsSectionSuspense videoId={videoId} />
       </ErrorBoundary>
@@ -20,18 +23,44 @@ export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
   );
 };
 
+const CommentsSectionSkeleton = () => {
+  return (
+    <div className='mt-6 flex items-center justify-center'>
+      <Loader2Icon className='text-muted-foreground size-7 animate-spin' />
+    </div>
+  );
+};
+
 export const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
-  const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
+  const [comments, query] = trpc.comments.getMany.useSuspenseInfiniteQuery(
+    {
+      videoId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: lastPage => lastPage.nextCursor,
+    },
+  );
 
   return (
     <div className='mt-6'>
       <div className='flex flex-col gap-6'>
-        <h1>0 comments</h1>
+        <h1 className='text-xl font-bold'>
+          {comments.pages[0].totalCount} Comments
+        </h1>
         <CommentForm videoId={videoId} />
         <div className='flex flex-col gap-4 mt-2'>
-          {comments.map(comment => (
-            <CommentItem key={comment.id} comment={comment} />
-          ))}
+          {comments.pages
+            .flatMap(page => page.items)
+            .map(comment => (
+              <CommentItem key={comment.id} comment={comment} />
+            ))}
+          <InfiniteScroll
+            isManual
+            hasNextPage={query.hasNextPage}
+            isFetchingNextPage={query.isFetchingNextPage}
+            fetchNextPage={query.fetchNextPage}
+          />
         </div>
       </div>
     </div>
