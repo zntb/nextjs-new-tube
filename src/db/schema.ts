a86f8a16f937636e550e073e1e89fa932e8203a3
zntb <zntbdev@proton.me>
2025-03-27 16:15:31 +0200

    Add playlists schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,6 +18,60 @@ import {
 
 export const reactionType = pgEnum('reaction_type', ['like', 'dislike']);
 
+export const playlistVideos = pgTable(
+  'playlist_videos',
+  {
+    playlistId: uuid('playlist_id')
+      .references(() => playlists.id, {
+        onDelete: 'cascade',
+      })
+      .notNull(),
+    videoId: uuid('video_id')
+      .references(() => videos.id, {
+        onDelete: 'cascade',
+      })
+      .notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  t => [
+    primaryKey({
+      name: 'playlist_videos_pk',
+      columns: [t.playlistId, t.videoId],
+    }),
+  ],
+);
+
+export const playlistVideoRelations = relations(playlistVideos, ({ one }) => ({
+  playlist: one(playlists, {
+    fields: [playlistVideos.playlistId],
+    references: [playlists.id],
+  }),
+  video: one(videos, {
+    fields: [playlistVideos.videoId],
+    references: [videos.id],
+  }),
+}));
+
+export const playlists = pgTable('playlists', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
+  description: text('description'),
+  userId: uuid('user_id')
+    .references(() => users.id, { onDelete: 'cascade' })
+    .notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const playlistRelations = relations(playlists, ({ one, many }) => ({
+  user: one(users, {
+    fields: [playlists.userId],
+    references: [users.id],
+  }),
+  playlistVideos: many(playlistVideos),
+}));
+
 export const users = pgTable(
   'users',
   {
@@ -44,6 +98,7 @@ export const userRelations = relations(users, ({ many }) => ({
   }),
   comments: many(comments),
   commentReactions: many(commentReactions),
+  playlists: many(playlists),
 }));
 
 export const subscriptions = pgTable(
@@ -146,6 +201,7 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
   views: many(videoViews),
   reactions: many(videoReactions),
   comments: many(comments),
+  playlistVideos: many(playlistVideos),
 }));
 
 export const comments = pgTable(
