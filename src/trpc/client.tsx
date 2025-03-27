3c7937c5306c47c277c2b13aa586bd3d27e474a3
zntb <zntbdev@proton.me>
2025-03-27 09:30:21 +0200

    Update App URL

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -23,9 +23,9 @@ function getQueryClient() {
 function getUrl() {
   const base = (() => {
     if (typeof window !== 'undefined') return '';
-    // TODO: Modify for outside-Vercel deployment
-    if (APP_URL) return `https://${APP_URL}`;
-    return 'http://localhost:3000';
+
+    // Crucial to modify in .env to production domain(including protocol)
+    return APP_URL;
   })();
   return `${base}/api/trpc`;
 }
