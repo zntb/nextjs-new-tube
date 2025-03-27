4d03b7fb2e9eda676aa6394b115443dd3e430d1e
zntb <zenetibi@yahoo.com>
2025-03-27 16:26:13 +0200

    Add playlists view

---
+++ src/app/(home)/playlists/page.tsx
@@ -0,0 +1,11 @@
+import { PlaylistsView } from '@/modules/playlists/ui/views/playlists-view';
+
+const PlaylistsPage = () => {
+  return (
+    <div>
+      <PlaylistsView />
+    </div>
+  );
+};
+
+export default PlaylistsPage;
