d05057abd5084164a118413ed2f5aaddb19ebbcb
zntb <zenetibi@yahoo.com>
2025-03-29 12:01:58 +0200

    Implement Banner upload

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -1,9 +1,13 @@
+'use client';
+
+import { useState } from 'react';
 import { cn } from '@/lib/utils';
 import { UserGetOneOutput } from '../../types';
 import { useAuth } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import { Edit2Icon } from 'lucide-react';
 import { Skeleton } from '@/components/ui/skeleton';
+import { BannerUploadModal } from './banner-upload-modal';
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
@@ -15,10 +19,15 @@ export const UserPageBannerSkeleton = () => {
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
   const { userId } = useAuth();
+  const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false);
 
   return (
     <div className='relative group'>
-      {/* TODO: Add upload banner modal */}
+      <BannerUploadModal
+        userId={user.id}
+        open={isBannerUploadModalOpen}
+        onOpenChange={setIsBannerUploadModalOpen}
+      />
       <div
         className={cn(
           'w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl',
@@ -32,6 +41,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
       >
         {user.clerkId === userId && (
           <Button
+            onClick={() => setIsBannerUploadModalOpen(true)}
             type='button'
             size='icon'
             className='absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300'
